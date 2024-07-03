146b143d98bef69077f4ef150c66975fa054da12
goot72 <157259472+goot72@users.noreply.github.com>
2024-07-02 17:43:51 -0700

    added footer links

--- reactPortfolio/src/App.jsx
+++ reactPortfolio/src/App.jsx
@@ -19,7 +19,10 @@ function App() {
             <Contact />
         </main>
         <footer className='footer'>
-            <button className="footer__links">Git Hub</button>
+            <a href="https://github.com/goot72" className="button__link">Git Hub</a>
+            <a href="https://www.linkedin.com/in/aaron-gutierrez-13a6a7317/" className="button__link">linkedin</a>
+            <a href="https://www.instagram.com/aarongutierrez_72/?igsh=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr" className="button__link">Instagram</a>
+
         </footer>
         </>
 
