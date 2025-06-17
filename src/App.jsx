2be714f6a81b72ed6e0c6f67b33644c81c627649
goot72 <157259472+goot72@users.noreply.github.com>
2025-06-16 20:17:57 -0700

    added discord link

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
             <a href="https://github.com/goot72" className="button__link">Git Hub</a>
             <a href="https://www.linkedin.com/in/aaron-gutierrez-13a6a7317/" className="button__link">linkedin</a>
             <a href="https://www.instagram.com/aarongutierrez_72/?igsh=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr" className="button__link">Instagram</a>
+            <a href="" className="button_link">Discord</a>
 
         </footer>
         </>
